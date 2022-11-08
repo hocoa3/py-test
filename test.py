a0d7f1886d062b3be918e7dbbbe5bc4908b2297b
lby <735827730@qq.com>
2022-11-08 20:04:05 +0800

    增加了改文件后缀

--- test.py
+++ test.py
@@ -3,38 +3,15 @@ import os
 import os.path
 
 
-dir='D:/GreemBang作品合集/GreemBang/[Patreon] Greem Bang -2021年2月'
+dir='C:/Users/86178/BaiduNetDisk'
 save_path='D:/GreemBang作品合集/GreemBang/[Patreon] Greem Bang -2021年2月'
 
 def test():
-  year_a=2020
-  year=2020
-  month=12
-  while(year>2019 and month>1):
-    month_a=month-4
-    if(month==1):
-      month=12
-      year=year-1
-    elif(month_a<1):
-      month_a=12
-      year_a=year_a-1
-    if(month<10):
-      path=save_path+'/'+str(year)+'.0'+str(month)
-
-    else:
-      path=save_path+'/'+str(year)+'.'+str(month)
-
-    if(month_a<10):
-  
-      path_a=save_path+'/'+str(year_a)+'.0'+str(month_a)
-    else:
-
-      path_a=save_path+'/'+str(year_a)+'.'+str(month_a)
-    os.renames(path_a,path)
-    month=month-1
-
+  for file in os.listdir(dir):                        #统一修改文件后缀名
+    if(re.search('iso',file)):
+      os.rename(dir+'/'+file,dir+'/'+file[:-3]+'rar')
    
-def Bring_All(dir,save_path):
+def Bring_All(dir,save_path):                         #将各个小文件夹全部取出放到存储路径
     for file in os.listdir(dir):
       if(os.path.isdir(dir+'/'+file)):
         dir1=dir+'/'+file
@@ -55,7 +32,7 @@ def Bring_All(dir,save_path):
         os.rename(dir+'/'+file,dir+'/'+file[:fir]+file[las:])         '''
 
 
-def Keep_Name(dir,save_path):
+def Keep_Name(dir,save_path):                         #保存小文件夹名字到下一层的文件然后取出到存储路径
     for file in os.listdir(dir):
       if(os.path.isdir(dir+'/'+file)):
         dir1=dir+'/'+file
@@ -69,9 +46,9 @@ def Keep_Name(dir,save_path):
         os.rename(dir+'/'+file,save_path+'/'+name)
 
 def main():
-  #test()
+  test()
   #Bring_All(dir,save_path)
-  Keep_Name(dir,save_path)
+  #Keep_Name(dir,save_path)
 
 if __name__=='__main__':
     main()
