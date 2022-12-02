f917294b366c4f85c808bc93945c64249229c391
lby <735827730@qq.com>
2022-12-02 17:07:34 +0800

    自动生成明细表功能基本完善,但不能生成两页,也不能排序

--- ExcelTest.py
+++ ExcelTest.py
@@ -35,6 +35,14 @@ class Alphabet(Enum):
     Y=25
     Z=26
 
+class Component():
+    def __init__(self,mark,footprint,volume,num,quantity):
+        self.mark=mark
+        self.footprint=footprint
+        self.volume=volume
+        self.num=num
+        self.quantity=quantity
+
 class BOM_List():
     def __init__(self,filename):
         dir=r'C:\Users\86178\Desktop'
@@ -83,39 +91,93 @@ class Template_List(BOM_List):
     def Write_OnebyColumn_mark(self,list):
         component_pos='C8'
         pos=component_pos
+        off=0
         for x in range(len(list)):
-            self.Write_One_Block(pos,list[x])
-            pos=pos[0]+str(x+8+1)
+            num=len(list[x].split(', '))
+            if(num>=4):
+                name=list[x].split(', ')
+                
+                while(num>4):
+                    dat=name[0]+', '+name[1]+', '+name[2]+', '+name[3]+', '
+                    self.Write_One_Block(pos,dat)
+                    off=off+1
+                    pos=pos[0]+str(x+8+off)
+                    num-=4
+                    name=name[4:]
+                if(len(name)==1):
+                    dat=name[0]
+                else:
+                    dat=''
+                    for x in range(len(name)):
+                        dat=dat+name[x]+', '
+                    dat=dat[:-2]
+                self.Write_One_Block(pos,dat)
+                print('pos1%s'% pos)
+            else:
+                pos=pos[0]+str(x+8+off)
+                self.Write_One_Block(pos,list[x])
+                print('pos2%s'% pos)
+            pos=pos[0]+str(x+8+1+off)
+        print(pos)
         self.Save()
 
     def Write_OnebyColumn_Quantity(self,list):
         component_pos='G8'
         pos=component_pos
+        off=0
         for x in range(len(list)):
+            num=list[x]
             self.Write_One_Block(pos,list[x])
-            pos=pos[0]+str(x+8+1)
+            while(num>4):
+                pos=pos[0]+str(x+8+1+off)
+                off+=1
+                self.Write_One_Block(pos,None)
+                num-=4
+            pos=pos[0]+str(x+8+1+off)
         self.Save()
 
-    def Write_OnebyColumn_number(self,list):
+    def Write_OnebyColumn_number(self,list,quantity):
         component_pos='B4'
         pos=component_pos
-        for x in range(1,len(list)+4):
+        off=0
+        idx=0
+        num=0
+        for x in range(1,len(list)+5):
+            if(x>4):
+                num=quantity[idx]
             self.Write_One_Block(pos,x)
-            pos=pos[0]+str(x+4+1)
+            while(num>4):
+                pos=pos[0]+str(x+4+off)
+                off+=1
+                self.Write_One_Block(pos,None)
+                num-=4
+            pos=pos[0]+str(x+4+off)
+            if(x>4):
+                idx+=1
+            print(x)
         self.Save()
     
-    def Write_OnebyColumn_unit(self,list):
+    def Write_OnebyColumn_unit(self,list,quantity):
         component_pos='F8'
         pos=component_pos
-        for x in range(1,len(list)):
+        off=0
+        for x in range(0,len(list)):
+            num=quantity[x]
             self.Write_One_Block(pos,'个')
-            pos=pos[0]+str(x+8+1)
+            while(num>4):
+                pos=pos[0]+str(x+8+1+off)
+                off+=1
+                self.Write_One_Block(pos,None)
+                num-=4
+            pos=pos[0]+str(x+8+1+off)
         self.Save()
 
-    def Write_OnebyColumn_VolumeandFootPrint(self,list_V,list_F):
+    def Write_OnebyColumn_VolumeandFootPrint(self,list_V,list_F,quantity):
         component_pos='E8'
         pos=component_pos
+        off=0
         for x in range(len(list_V)):
+            num=quantity[x]
             if(re.search('F',str(list_V[x])) and re.match(r'\d{4}',str(list_F[x]))):
                 name='贴片电容'+str(list_F[x])+'-'+str(list_V[x])
             elif(re.search('F',str(list_V[x])) and re.match(r'\d{3}',str(list_F[x]))):
@@ -127,9 +189,14 @@ class Template_List(BOM_List):
             elif(re.match(r'\d{3}',str(list_F[x]))):
                 name='贴片电阻0'+str(list_F[x])+'-'+str(list_V[x])+'Ω'
             else:
-                name=str(list_V[x])
+                name=str(list_V[x])  
             self.Write_One_Block(pos,name)
-            pos=pos[0]+str(x+8+1)
+            while(num>4):
+                pos=pos[0]+str(x+8+1+off)
+                off+=1
+                self.Write_One_Block(pos,None)
+                num=num -4
+            pos=pos[0]+str(x+8+1+off)
         self.Save()
     
     
@@ -139,7 +206,8 @@ class Template_List(BOM_List):
         print('save done')
 
 if __name__=='__main__':
-    name1='BOM_Test.xlsx'                                                   #物料清单源文件名字在此输入
+    name1='BOM_Test.xlsx'     
+    #物料清单源文件名字在此输入
     BOM1=BOM_List(name1)
     #dat=BOM1.Read_One_Block('C8')
     mark_list=BOM1.Read_One_Column(4)
@@ -148,11 +216,12 @@ if __name__=='__main__':
     quantity_list=BOM1.Read_One_Column(3)
     Template=Template_List()
 
-    Template.Write_OnebyColumn_mark(mark_list)
-    Template.Write_OnebyColumn_VolumeandFootPrint(volume_list,footprint_list)
-    Template.Write_OnebyColumn_Quantity(quantity_list)
-    Template.Write_OnebyColumn_number(mark_list)
-    Template.Write_OnebyColumn_unit(mark_list)
+    Template.Write_OnebyColumn_mark(mark_list)                                                #处理下标
+    Template.Write_OnebyColumn_VolumeandFootPrint(volume_list,footprint_list,quantity_list)   #处理封装和大小
+    Template.Write_OnebyColumn_Quantity(quantity_list)                                        #处理数量
+    Template.Write_OnebyColumn_number(mark_list,quantity_list)                                #处理序号
+    Template.Write_OnebyColumn_unit(mark_list,quantity_list)                                  #处理单位
+    
     print('done')
 
     
